5b3e35f80fbb5b91211bbaba68253c716939a55d
okome-program <okome-program@outlook.jp>
2026-06-14 13:39:42 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -2,10 +2,28 @@ import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 3000 });
 
-wss.on("connection", (ws) => {
-    ws.send("接続成功！");
+const room = [];
 
-    ws.on("message", (msg) => {
-        ws.send("受け取った！: " + msg);
+wss.on("connection", (ws) => {
+  if (room.length >= 2) {
+    ws.send("サーバーが満員です");
+    ws.close();
+    return;
+  }
+
+  room.push(ws);
+  ws.send("サーバーに参加しました");
+
+  ws.on("message", (msg) => {
+    room.forEach(client => {
+      if (client !== ws && client.readyState === 1) {
+        client.send(msg);
+      }
     });
+  });
+
+  ws.on("close", () => {
+    const i = room.indexOf(ws);
+    if (i !== -1) room.splice(i, 1);
+  });
 });
